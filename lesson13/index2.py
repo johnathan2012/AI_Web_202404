4956a3a68b7c88e1598aa3a546196597e569a0ce
陳添盛 <chen.johnathan@gmail.com>
2024-06-29 03:52:48 +0000

    f

--- lesson13/index2.py
+++ lesson13/index2.py
@@ -16,11 +16,15 @@ else:
 
     areas:list[str] = list(set(map(lambda value:value['行政區'],data)))
 
+    def area_change():
+        print("Hello!")
+
     # option = st.selectbox("請選擇行政區",areas)
     # st.write("您選擇:", option)
 
     with st.sidebar:
-        st.selectbox(":orange[請選擇行政區]",options=areas)
+        st.selectbox(":orange[請選擇行政區]",options=areas,on_change=area_change,key='sarea')
+        st.session_state
 
 
 
